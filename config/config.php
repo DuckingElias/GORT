5f720271efac79f84cd4cdb2b73edb9744cfbcd9
Elias Kleppinger <eliaskleppinger@pm.me>
2021-08-22 13:00:46 +0200

    Added config.

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+DEFINE("DEBUG", true);
+
+DEFINE("URL_PATH", "/gort");
+
+DEFINE("MYSQL_URI", "mysql:host=localhost;dbname=test");
+DEFINE("MYSQL_USERNAME", "test");
+DEFINE("MYSQL_PASSWORD", "");
